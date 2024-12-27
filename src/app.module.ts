d6e5dafb5792c660afdf1977726517a09f2192c9
DevHubOneOfficial <vasil@devhubone.com>
2024-12-27 13:24:16 +0200

    added health, jwt auth and users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { HealthModule } from './health/health.module';
+import { S3Module } from './s3/s3.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [HealthModule, S3Module, UserModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
